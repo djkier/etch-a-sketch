f7f86923735bc3b22341cd27c291cc671403e3f6
djkier <djjustinekier@gmail.com>
2024-08-03 15:21:46 +0800

    Feat: Eraser button, clear button. FIX: Auto button works

--- script/event.js
+++ script/event.js
@@ -5,9 +5,15 @@ const drawingBoard = document.querySelector("#drawing-board")
 const colorPicker = document.querySelector('#color-picker');
 const colorCurrent = document.querySelector('#current-color');
 
-const divPerSide = gridSize.value;
+const eraserBtn = document.querySelector('#eraser-button');
+const clearBtn = document.querySelector('#clear-button')
+
+const autoDrawBtn = document.querySelector('#auto-draw');
+
+let divPerSide = gridSize.value;
 let colorPicked = colorPicker.value;
-let action = 'mousemove';
+let autoClicked = true;
+let eraserClicked = false;
 
 
 function windowLoad(divNum) {
@@ -16,37 +22,80 @@ function windowLoad(divNum) {
         const newDiv = drawingBoard.appendChild(document.createElement("div"));
         newDiv.style.height = `${newDivSide}px`;
         newDiv.style.width = `${newDivSide}px`;
-
-    }
-
-    
-
-    drawingBoard.addEventListener(action, (e) => {
-            if (e.target.id !== drawingBoard.id){
-                e.target.style.backgroundColor = colorPicked;
-        }
-    })
+    } 
 }
 
+function btnClickEffectOn(elem) {
+    elem.style.backgroundColor = 'black';
+    elem.style.color = 'white';
+}
 
+function btnClickEffectOff(elem) {
+    elem.style.backgroundColor = 'white';
+    elem.style.color = 'black';
+}
+
+function refreshGrid(val){
+    drawingBoard.replaceChildren();
+    windowLoad(val)
+}
 
 colorPicker.addEventListener('change', (e) => {
     colorCurrent.style.backgroundColor = e.target.value;
     colorPicked = e.target.value;
 })
 
+eraserBtn.addEventListener('click', () => {
+    if (!eraserClicked) {
+        colorPicked = '#eeeeee';
+        btnClickEffectOn(eraserBtn);
+       
+    } else {
+        colorPicked = colorPicker.value;
+        btnClickEffectOff(eraserBtn);
+        
+    }
+
+    eraserClicked = !eraserClicked;
+})
+
+clearBtn.addEventListener('click', () => {
+    refreshGrid(divPerSide);
+})
+
+
 gridSize.addEventListener('input', (e) => {
     gridSizeNum.textContent = `${e.target.value}X${e.target.value}`;
 })
 
-
 gridSize.addEventListener('change', (e) => {
-    drawingBoard.replaceChildren();
-    windowLoad(e.target.value);
+    refreshGrid(e.target.value);
+    divPerSide = e.target.value;
 })
 
+
+
+const mouseOverEvent = (e) => {
+    if (e.target.id !== drawingBoard.id){
+        e.target.style.backgroundColor = colorPicked; 
+    }
+}
+
+
+autoDrawBtn.addEventListener('click', () => {
+    if (autoClicked) {
+        btnClickEffectOff(autoDrawBtn);
+        drawingBoard.removeEventListener('mousemove', mouseOverEvent )
+    } else {
+        btnClickEffectOn(autoDrawBtn);
+        drawingBoard.addEventListener('mousemove', mouseOverEvent);
+    }
+
+    autoClicked = !autoClicked;
+})
 
 
 colorCurrent.style.backgroundColor = colorPicker.value;
 gridSizeNum.textContent = `${gridSize.value}X${gridSize.value}`;
 windowLoad(divPerSide);
+drawingBoard.addEventListener('mousemove', mouseOverEvent);
