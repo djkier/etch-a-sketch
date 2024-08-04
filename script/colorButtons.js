60cdd417929d125d975b35f6f247f171a4c32f4c
djkier <djjustinekier@gmail.com>
2024-08-04 18:49:15 +0800

    Feat: grayscale, and rainbow working

--- script/colorButtons.js
+++ script/colorButtons.js
@@ -1,3 +1,4 @@
+
 const colorCurrent = document.querySelector('#current-color');
 
 const colorPicker = document.querySelector('#color-picker');
@@ -6,9 +7,29 @@ const rainbowBtn = document.querySelector('#rainbow-button');
 const artisticBtn = document.querySelector('#artistic-button');
 const eraserBtn = document.querySelector('#eraser-button');
 
+const drawingBoard = document.querySelector("#drawing-board");
+
+
+
+
 
 let colorPicked = colorPicker.value;
+let currentColor = colorPicker.value;
+let grayClicked = false;
+let rainbowClicked = false;
 let eraserClicked = false;
+
+function disableBtn(elem) {
+    elem.style.backgroundColor = 'gray';
+    elem.style.cursor = 'not-allowed';
+    elem.disabled = true;
+}
+
+function enableBtn(elem) {
+    elem.style.backgroundColor = 'white';
+    elem.style.cursor = 'pointer';
+    elem.disabled = false;
+}
 
 function btnClickEffectOn(elem) {
     elem.style.backgroundColor = 'black';
@@ -20,9 +41,74 @@ function btnClickEffectOff(elem) {
     elem.style.color = 'black';
 }
 
-colorPicker.addEventListener('change', (e) => {
-    colorCurrent.style.backgroundColor = e.target.value;
+function colorRandomEffects(btnfn, elem, gradient, fn){
+    btnfn(elem);
+    colorCurrent.style.background = gradient;
+    fn(artisticBtn);
+    fn(colorPicker);
+}
+
+
+
+
+const rainbowEffects = () => {
+    colorPicked = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+   
+}
+
+const grayEffects = () => {
+    const randColor = Math.floor(Math.random()*255);
+    colorPicked = `rgb(${randColor}, ${randColor}, ${randColor})`;
+}
+
+
+
+colorPicker.addEventListener('change', e => {
     colorPicked = e.target.value;
+    colorCurrent.style.background = e.target.value;
+});
+
+
+
+
+
+grayScaleBtn.addEventListener('click', ()=> {
+    if (rainbowClicked) {
+        drawingBoard.removeEventListener('mouseout', rainbowEffects);
+        btnClickEffectOff(rainbowBtn);
+        rainbowClicked = !rainbowClicked;
+    }
+    
+    const grayScaleGradient = `linear-gradient(111deg, rgba(33,33,33,1) 0%, rgba(228,228,228,1) 100%)`;
+    if (!grayClicked) {
+        drawingBoard.addEventListener('mouseout', grayEffects);
+        colorRandomEffects(btnClickEffectOn, grayScaleBtn, grayScaleGradient, disableBtn);
+    } else {
+        drawingBoard.removeEventListener('mouseout', grayEffects);
+        colorRandomEffects(btnClickEffectOff, grayScaleBtn, currentColor, enableBtn);
+        colorPicked = currentColor;
+    }
+    grayClicked = !grayClicked;
+})
+
+rainbowBtn.addEventListener('click', () => {
+    if (grayClicked) {
+        drawingBoard.removeEventListener('mouseout', grayEffects);
+        btnClickEffectOff(grayScaleBtn);
+        grayClicked = !grayClicked;
+    }
+
+
+    const rbScaleGradient = `linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)`;
+    if (!rainbowClicked) {
+        drawingBoard.addEventListener('mouseout', rainbowEffects);
+        colorRandomEffects(btnClickEffectOn, rainbowBtn, rbScaleGradient, disableBtn);
+    } else {
+        drawingBoard.removeEventListener('mouseout', rainbowEffects);
+        colorRandomEffects(btnClickEffectOff, rainbowBtn, currentColor, enableBtn);
+        colorPicked = currentColor;
+    }
+    rainbowClicked = !rainbowClicked;
 })
 
 
@@ -40,17 +126,15 @@ eraserBtn.addEventListener('click', () => {
 })
 
 
+
 colorCurrent.style.backgroundColor = colorPicker.value;
 
 
-function disabledBtn(elem) {
-    elem.style.backgroundColor = 'gray';
-    elem.style.cursor = 'not-allowed';
-}
-
-disabledBtn(grayScaleBtn);
-disabledBtn(rainbowBtn);
-disabledBtn(artisticBtn);
 
 
-export { colorPicked, btnClickEffectOff, btnClickEffectOn }
+// disabledBtn(grayScaleBtn);
+// disabledBtn(rainbowBtn);
+// disabledBtn(artisticBtn);
+
+
+export { colorPicked, btnClickEffectOff, btnClickEffectOn, drawingBoard }
